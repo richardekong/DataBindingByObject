e0e6c4f2c6feb9da143d2ab1119400850d127dd4
richard <richardekong290@gmail.com>
2021-12-19 06:24:47 +0100

    add all project file

---
+++ app/src/main/java/com/example/databindingbyobject/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.databindingbyobject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databindingbyobject.databinding.ActivityMainBinding
+import com.example.databindingbyobject.model.Student
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.student = createStudent()
+    }
+
+    private fun createStudent(): Student {
+        return Student("richardekong290@gmail.com", "Richard",29)
+    }
+}
